aa05c498359d4fa914b4f60e4c9660879f5c840d
Blake Georgii <blakegeorgii@gmail.com>
2022-01-11 12:59:04 -0800

    added player health methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -10,11 +10,9 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
-});
 
-expect(player.inventory).toEqual(
-    expect.arrayContaining([expect.any(Object)])
-);
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
 
 test("gets player's stats as an object", () => {
     const player = new Player('Dave');
@@ -34,3 +32,32 @@ test('gets inventory from player or returns false', () => {
 
     expect(player.getInventory()).toEqual(false);
 });
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+})
